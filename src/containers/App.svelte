348720f58d277e0db219915c2c42bd9503afe6ac
capulusnoctis <axel.solares.g@gmail.com>
2020-11-03 00:09:02 -0600

    Added: configuration files for development and production

---
+++ src/containers/App.svelte
@@ -0,0 +1,11 @@
+<style>
+    :global(body) {
+        padding: 0;
+        margin: 0;
+    }
+</style>
+
+<div class="main">
+    <h1>SEKO</h1>
+    <h2>Developing Experiences...</h2>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.mjs', '.svelte', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.svelte?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'svelte-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        open: true,
+        port: 9000
+    }
+}
